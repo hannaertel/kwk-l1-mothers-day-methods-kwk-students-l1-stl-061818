9e61f459c019cd1cc912f1d562a776761589f3f9
hannaertel <hannaertel@yahoo.com>
2018-12-31 16:54:54 +0000

    Automatically backed up by Learn

---
+++ holiday_greeting.rb
@@ -0,0 +1,4 @@
+def holiday_greeting(holiday = "Happy Mother's Day", to = "Mom", from = "Your Favorite Child")
+  puts "Happy #{holiday}, #{to}! From #{from}."
+end
+holiday_greeting("Thanksgiving", "Ron", "Harry")
