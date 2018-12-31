ffdda8dcc0e1a4fb997d8fae7b2d5a69f8c00601
hannaertel <hannaertel@yahoo.com>
2018-12-31 17:00:59 +0000

    Automatically backed up by Learn

--- holidaygreeting.rb
+++ holidaygreeting.rb
@@ -2,4 +2,4 @@ def holiday_greeting(holiday = "Fourth of July", to = "Beyonce", from = "Jay-Z")
   puts "Happy #{holiday}, #{to}! From " + from.upcase
 end
 holiday_greeting
-holiday_greeting(holiday == holiday, to = "Jlo", from = "pitbull")
+holiday_greeting(holiday = "Christmas", to = "Jlo", from = "pitbull")
